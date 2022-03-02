38dcac4075a65d69f2a2ada6bdefc18d84dba863
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2022-03-02 13:47:14 -0800

    added our testing packages and modified test.js

---
+++ test/test.js
@@ -0,0 +1,18 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../src/index.js'
+
+//Assertion style
+chai.should();
+
+chai.use(chaiHttp); 
+
+
+
+describe ("Our tests", () => {
+
+  
+  //Our tests will be included here
+
+
+});
