ae0a7d2ebe9abccb08b35edf493f834b4708845e
robert <robertishimwe0@gmail.com>
2022-03-04 00:21:19 +0200

    #181414700 chore setup migration,model and user table

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
