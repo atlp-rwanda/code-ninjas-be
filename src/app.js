1216d3b2a79be79cc3a4ca7331d1e04669e69d96
robert <robertishimwe0@gmail.com>
2022-03-04 08:58:34 +0200

    #181414700 chore setup migration,model and user table

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import dbase from './database/config/database.js';
 import usersRoute from './routes/userRoute.js';
-import dotenv from 'dotenv';
+
 dotenv.config();
 
 dbase.authenticate().then(() => {
-    console.log('database connected......');
+  console.log('database connected......');
 });
 
 const app = express();
