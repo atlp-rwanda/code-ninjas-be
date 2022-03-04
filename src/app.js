de4fead644db39cc4db4ad3fb2d8d9dc50059913
procech20 <mupinoe@gmail.com>
2022-03-04 06:14:34 +0200

    chore(integrate-HoundCI): Integrate HoundCI with badge attached to readme
    chore(linting): setup HoundCI for code quality review and integrate HoundCI badge in the Readme file
    
    chore(code-quality): setup HoundCI for code quality review and integrate HoundCI badge in the Readme file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import 'colors';
 import dbase from './database/config/database';
 import usersRoute from './routes/userRoute';
 
 dotenv.config();
 
 dbase.authenticate().then(() => {
-  console.log('database connected......');
+  console.log('database connected......'.green.bgBlue);
 });
 
 const app = express();
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import UserController from '../controllers/userController.js';
-const router = express.Router();
+import { Router } from 'express';
+import UserController from '../controllers/userController';
+
+const router = Router();
 
 router.get('/user', UserController.getAllUsers);
 
