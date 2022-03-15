adfe45d49d89499086b14c5a808416440149656c
Israel <andelarwanda@Andelas-MacBook-Pro.local>
2022-03-15 09:14:26 +0200

    Added User Registration

--- src/database/migrations/20220309061357-create-user.js
+++ src/database/migrations/20220309061357-create-user.js
@@ -6,10 +6,10 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER
       },
       firstName: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING
       },
       lastName: {
         type: Sequelize.STRING,
@@ -31,15 +31,15 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE,
+        type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE,
-      },
+        type: Sequelize.DATE
+      }
     });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Users');
-  },
+  }
 };
