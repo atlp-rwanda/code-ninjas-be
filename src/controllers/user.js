f2fde3bb76d74c60fc303cc20d74b84c3e3dabcc
Mugisha Israel <58687698+itisWasp@users.noreply.github.com>
2022-03-15 09:48:47 +0200

    Update user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,18 +2,14 @@ import models from '../database/models';
 import createUserService from '../services/UserServices';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-<<<<<<< HEAD
 import { generateToken } from '../services/TokenService';
 import { checkUser } from '../helpers/CheckUser';
-=======
->>>>>>> a10e5b2 (Rebase from Develop)
 const { v4: uuidv4 } = require('uuid');
 const User = models.User;
 
 class UserController {
   static createUser = async (req, res) => {
     //check if a user is in the database
-<<<<<<< HEAD
     const check = {
       email: req.body.Email,
     };
@@ -23,11 +19,6 @@ class UserController {
 
     const emailExists = await checkUser(check);
 
-=======
-    const emailExists = await User.findOne({
-      where: { email: req.body.Email },
-    });
->>>>>>> a10e5b2 (Rebase from Develop)
     if (emailExists)
       return res.status(400).json({ message: 'Email already Exists' });
 
@@ -42,7 +33,6 @@ class UserController {
         email: req.body.Email,
         userName: req.body.UserName,
         password: hashedPassword,
-<<<<<<< HEAD
       });
 
       //create and Assign a token
@@ -62,22 +52,6 @@ class UserController {
       res.status(200).json({
         message: 'User created successfully',
         token: token,
-=======
-        userId: uuidv4(),
-      });
-
-      //create and Assign a token
-      const token = jwt.sign(
-        {
-          user: { id: user.userId, username: user.userName, email: user.email },
-        },
-        process.env.TOKEN_SECRET,
-        { expiresIn: process.env.TOKEN_EXPIRE }
-      );
-      res.header('Authorization', token);
-      res.status(200).json({
-        message: 'User created successfully',
->>>>>>> a10e5b2 (Rebase from Develop)
       });
     } catch (error) {
       res.status(400).json({ error: error.message });
