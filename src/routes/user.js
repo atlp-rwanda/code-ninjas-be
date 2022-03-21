97bc73238425c0cd4ff424ef922e99c0568f5d71
Israel <andelarwanda@Andelas-MacBook-Pro.local>
2022-03-21 09:22:45 +0200

    Rebased from develop

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import UserController from '../controllers/user';
 import UserValidation from '../validations/UserValidation';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import EmailValidation from '../middlewares/EmailValidation';
 
 const router = Router();
@@ -13,23 +11,5 @@ router.post(
   EmailValidation.checkEmail,
   UserController.createUser
 );
-=======
-
-const router = Router();
-
-router.post('/register', UserValidation.verifyUser, UserController.createUser);
->>>>>>> a10e5b2 (Rebase from Develop)
-=======
-import EmailValidation from '../middlewares/EmailValidation';
-
-const router = Router();
-
-router.post(
-  '/register',
-  UserValidation.verifyUser,
-  EmailValidation.checkEmail,
-  UserController.createUser
-);
->>>>>>> 107db34 (Rebased from develop)
 
 export default router;
